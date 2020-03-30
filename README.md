609ee6481ddc40ca335b92a98779fd37aafcbb2c
sachin746 <sachinbhadur9@gmail.com>
2020-03-30 21:28:15 +0530

    v3

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# practice_app
-its for practice
-ghtdrycfhgv
+### Recruitment Portal of IIIT Lucknow
+
+This is a web app used for the recruitment process of IIIT Lucknow.
--- models/user.js
+++ models/user.js
@@ -1,8 +1,29 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+var passportLocalMongoose = require("passport-local-mongoose");
 
-var UserSchema = new mongoose.schema({
-    email : {type : Email, required : [true, "can't be blank"]},
-    password : {type : String, required : [true, "can't be blank"], minlength: 8}
+var UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        minlength: 8
+    
+    }
 })
 
+UserSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model("User", UserSchema); 
